60e0620e1ec085e9cb5f0ff4f1a468f9b1517036
John Lemp <john@graavy.com>
2024-11-03 07:46:45 -0500

    I give up, disable mouse mode

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -3,8 +3,55 @@ require "nvchad.mappings"
 -- add yours here
 
 local map = vim.keymap.set
-
+local opts = { noremap = true, silent = true }  
 map("n", ";", ":", { desc = "CMD enter command mode" })
-map("i", "jk", "<ESC>")
+
+
+
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+
+-- Neovide-specific settings
+if vim.g.neovide then
+  map('n', '<D-s>', ':w<CR>') -- Save
+  map('v', '<D-c>', '"+y') -- Copy
+  map('n', '<D-v>', '"+P') -- Paste normal mode
+  map('v', '<D-v>', '"+P') -- Paste visual mode
+  map('c', '<D-v>', '<C-R>+') -- Paste command mode
+  map('i', '<D-v>', '<ESC>l"+Pli') -- Paste insert mode
+end
+
+
+-- vim.api.nvim_set_keymap('v', '<C-^>', '"+y', { noremap = true, silent = true }) -- [94;5u 
+-- vim.api.nvim_set_keymap('v', '<C-%>', '"+y', { noremap = true, silent = true }) -- [37;5u
+-- vim.api.nvim_set_keymap('v', '<C-&>', '"+P', { noremap = true, silent = true }) -- [38;5u
+-- vim.api.nvim_set_keymap('n', '<C-&>', '"+P', { noremap = true, silent = true }) -- [38;5u
+-- vim.api.nvim_set_keymap('i', '<C-&>', '"+P', { noremap = true, silent = true }) -- [38;5u
+
+
+
+-- https://github.com/neovide/neovide/issues/1263#issuecomment-1972013043  
+-- vim.keymap.set(
+--     {'n', 'v', 's', 'x', 'o', 'i', 'l', 'c', 't'},
+--     '<D-v>',
+--     function() vim.api.nvim_paste(vim.fn.getreg('+'), true, -1) end,
+--     { noremap = true, silent = true }
+-- )
+
+-- JSON Formatting
+map("n", "<leader>pj2", "<Esc>:%!json_xs -f json -t json-pretty<CR>:set filetype=json<CR>", opts)
+map("n", "<leader>pj3", "<Esc>:%!jq '.'<CR>:set filetype=json<CR>", opts)
+map("n", "<leader>pj4444", "<Esc>:%!python2 -m json.tool<CR>:set filetype=json<CR>", opts)
+
+-- Alternative JSON formatting using Ruby
+map("n", "<leader>pj", "<Esc>:%!ruby -rjson -e 'puts JSON.pretty_generate(JSON.load($<))'<CR>:set filetype=json<CR>", opts)
+
+-- YAML Formatting using Ruby
+map("n", "<leader>py", "<Esc>:%!ruby -ryaml -e 'puts YAML.load($<).to_yaml'<CR>:set filetype=yaml<CR>", opts)
+
+-- XML Formatting
+map("n", "<leader>px", "<Esc>:%!ruby -W0 ~/.vim/xmlformat.rb<CR>:set filetype=xml<CR>", opts)
+map("n", "<leader>px2", "<Esc>:%!~/.vim/xmlformat.pl<CR>:set filetype=xml<CR>", opts)
+
+-- HTML Formatting using tidy
+map("n", "<leader>ph", "<Esc>:%!tidy -q -i --wrap 120 --show-errors 0<CR>:set filetype=html<CR>", opts)
--- lua/options.lua
+++ lua/options.lua
@@ -3,4 +3,26 @@ require "nvchad.options"
 -- add yours here!
 
 -- local o = vim.o
--- o.cursorlineopt ='both' -- to enable cursorline!
+-- o.cursorlineopt ='both' -- to enable cursorline
+local opt = vim.opt
+opt.mouse = ""
+-- Core Settings
+opt.number = true                 -- Show line numbers
+opt.relativenumber = false         -- Relative line numbers
+opt.wrap = true                    -- Enable line wrapping
+opt.scrolloff = 3                  -- Keep 3 lines of context around cursor
+opt.tabstop = 2                    -- Set tab width to 2 spaces
+opt.shiftwidth = 2                 -- Indentation width of 2 spaces
+opt.expandtab = true               -- Use spaces instead of tabs
+opt.clipboard = "unnamedplus"      -- Use system clipboard
+opt.cursorline = true              -- Highlight current line
+opt.termguicolors = true           -- Enable true colors
+opt.hlsearch = true                -- Highlight search matches
+opt.incsearch = true               -- Incremental search
+opt.ignorecase = true              -- Case-insensitive search
+opt.smartcase = true               -- Case-sensitive if uppercase used
+
+if vim.g.neovide then
+  vim.g.neovide_cursor_vfx_mode = "railgun"  -- Optional, visual effect
+  vim.g.neovide_input_use_logo = true  -- Enable Cmd as <D->
+end
